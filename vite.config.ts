b9d83fe4848eb710644e61dcc00e47241f5589ac
ynlee-byte <ynlee1130@hanyang.ac.kr>
2025-10-21 13:46:36 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && screenGraphPlugin()],
   publicDir: "./static",
-  base: "./",
+  base: "main/",
   css: {
     postcss: {
       plugins: [tailwind()],
